9b47aab469487a63cdb5571a5afeb033949eb638
Magnus <magnubgr@uio.no>
2019-10-15 14:06:04 +0200

    Function now returns features and targets of dataset

--- code-src/data_reader.py
+++ code-src/data_reader.py
@@ -1,53 +1,61 @@
 import numpy as np
 import pandas as pd
 
-file = "../data/default_credit_card_data.xls"
 
-from pandas import ExcelWriter
-from pandas import ExcelFile
+def read_credit_card_file():
 
-# df = pd.read_excel(file, sheetname='Data')
-# df = pd.read_excel(file, header=[0, 1], sheetname="Data")
-# df = pd.read_excel(file, header=[0, 1], sheetname="Data")
-df = pd.read_excel(file, header=1, skiprows=0, index_col=0)
+    file = "../data/default_credit_card_data.xls"
 
-df.rename(
-    index=str,
-    columns={"default payment next month": "defaultPaymentNextMonth"},
-    inplace=True
-)
+    # df = pd.read_excel(file, header=[0, 1], sheetname="Data")
+    df = pd.read_excel(file, header=1, skiprows=0, index_col=0)
 
-print(df)
-print("Column headings:")
-columns = df.columns
-print(columns)
+    df.rename(
+        index=str,
+        columns={"default payment next month": "defaultPaymentNextMonth"},
+        inplace=True
+    )
 
-id_labels = df.index
+    columns_df = df.columns
+    columns_np = columns_df.values
 
-# limit_bal = df['X1']
-# sex = df['X2']
-# education = df['X3']
-# marriage = df['X4']
-# age = df['X5']
-# pay_0 = df['X6']
-# pay_2 = df['X7']
-# pay_3 = df['X8']
-# pay_4 = df['X9']
-# pay_5 = df['X10']
-# pay_6 = df['X11']
-# bill_amt1 = df['X12']
-# bill_amt2 = df['X13']
-# bill_amt3 = df['X14']
-# bill_amt4 = df['X15']
-# bill_amt5 = df['X16']
-# bill_amt6 = df['X17']
-# pay_amt1 = df['X18']
-# pay_amt2 = df['X19']
-# pay_amt3 = df['X20']
-# pay_amt4 = df['X21']
-# pay_amt5 = df['X22']
-# pay_amt6 = df['X23']
-# default = df['Y']
+    id_labels_df = df.index
+    id_labels_np = id_labels_df.values
 
+    X = df.loc[:, df.columns != 'defaultPaymentNextMonth'].values
+    y = df.loc[:, df.columns == 'defaultPaymentNextMonth'].values
+    # print(X)
+    # print(y)
 
-# print(df)
+    return X, y
+
+    """ This is in case we need the individual columns
+    limit_bal = df['LIMIT_BAL']
+    sex = df['SEX']
+    education = df['EDUCATION']
+    marriage = df['MARRIAGE']
+    age = df['AGE']
+    pay_0 = df['PAY_0']
+    pay_2 = df['PAY_2']
+    pay_3 = df['PAY_3']
+    pay_4 = df['PAY_4']
+    pay_5 = df['PAY_5']
+    pay_6 = df['PAY_6']
+    bill_amt1 = df['BILL_AMT1']
+    bill_amt2 = df['BILL_AMT2']
+    bill_amt3 = df['BILL_AMT3']
+    bill_amt4 = df['BILL_AMT4']
+    bill_amt5 = df['BILL_AMT5']
+    bill_amt6 = df['BILL_AMT6']
+    pay_amt1 = df['PAY_AMT1']
+    pay_amt2 = df['PAY_AMT2']
+    pay_amt3 = df['PAY_AMT3']
+    pay_amt4 = df['PAY_AMT4']
+    pay_amt5 = df['PAY_AMT5']
+    pay_amt6 = df['PAY_AMT6']
+    default = df['defaultPaymentNextMonth']
+    """
+
+    # print(df)
+
+if __name__ == "__main__":
+    read_credit_card_file()
