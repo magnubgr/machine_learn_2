a48ee080626213a522cb0b1cf14249ef923dbdef
Magnus Grøndalen <magnus.bg@hotmail.com>
2019-10-23 00:56:21 +0200

    Added check for feature space, and scaling of features

--- code-src/data_reader.py
+++ code-src/data_reader.py
@@ -1,19 +1,24 @@
 import numpy as np
 import pandas as pd
 import sklearn
+import os
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, RobustScaler #, OneHotEncoder
 
 ## Also xlrd is needed
 
 def read_credit_card_file():
 
-    file = "../data/default_credit_card_data.xls" # fix this pointer
+    path = os.path.dirname(os.path.realpath(__file__))
+    file = path + "/../data/default_credit_card_data.xls" # fix this pointer
 
     # df = pd.read_excel(file, header=[0, 1], sheetname="Data")
     df = pd.read_excel(file, header=1, skiprows=0, index_col=0)
 
     df.rename(
         index=str,
-        columns={"default payment next month": "default"},
+        columns={"default payment next month": "DEFAULT"},
         inplace=True
     )
 
@@ -22,15 +27,24 @@ def read_credit_card_file():
 
 
     ### Check if df["SEX"] has other values than 1,2
-    print( np.unique( df["SEX"].values) )
+    print("SEX", np.unique( df["SEX"].values) )
     ### Check if df["EDUCATION"] has other values than 1,2,3,4 ---- apparently 0,5,6
-    print( np.unique( df["EDUCATION"].values) )
+    print("EDUCATION", np.unique( df["EDUCATION"].values) )
     ### Check if df["MARRIAGE"] has other values than 1,2,3    ---- apparently 0
-    print( np.unique( df["MARRIAGE"].values) )
+    print("MARRIAGE", np.unique( df["MARRIAGE"].values) )
+    ### Check if df["PAY_0"] has other values than -1,..,9
+    print("PAY_0", np.unique( df["PAY_0"].values) )
+    ### Check if df["PAY_2"] has other values than -1,..,9
+    print("PAY_2", np.unique( df["PAY_2"].values) )
+    ### Check if df["PAY_4"] has other values than -1,..,9
+    print("PAY_4", np.unique( df["PAY_4"].values) )
+    ### Check if df["PAY_6"] has other values than -1,..,9
+    print("PAY_6", np.unique( df["PAY_6"].values) )
 
     ## Consider one-hot encoding PAY_0-PAY_6... but lots of work
     ### What does 0 mean?
     #### Test out using just PAYed on time or not (0, 1)
+
 
     # One-hot encoding the gender
     df["MALE"] = (df["SEX"]==1).astype("int")
@@ -47,7 +61,7 @@ def read_credit_card_file():
     df["SINGLE"] = (df["MARRIAGE"]==2).astype("int")
     df.drop("MARRIAGE", axis=1, inplace=True)
 
-    # display(df)
+    display(df)
 
 
 
@@ -57,42 +71,26 @@ def read_credit_card_file():
     # id_labels_df = df.index
     # id_labels_np = id_labels_df.values
 
+
     X = df.loc[:, df.columns != 'defaultPaymentNextMonth'].values
     y = df.loc[:, df.columns == 'defaultPaymentNextMonth'].values
-    # print(np.shape(X))
-    # print(np.shape(y))
+
+    ## Scale the features. So that for example LIMIT_BAL isnt larger than AGE 
+    standard_scaler = StandardScaler()
+    # robust_scaler = RobustScaler()        # RobustScaler ignores outliers 
+    X = standard_scaler.fit_transform(X)
+
+
+
+
+    # Train-test split
+    test_size = 0.3
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=4)
+
+
 
     return X, y
 
-'''
-    This is in case we need the individual columns
-    limit_bal = df['LIMIT_BAL']
-    sex = df['SEX']
-    education = df['EDUCATION']
-    marriage = df['MARRIAGE']
-    age = df['AGE']
-    pay_0 = df['PAY_0']
-    pay_2 = df['PAY_2']
-    pay_3 = df['PAY_3']
-    pay_4 = df['PAY_4']
-    pay_5 = df['PAY_5']
-    pay_6 = df['PAY_6']
-    bill_amt1 = df['BILL_AMT1']
-    bill_amt2 = df['BILL_AMT2']
-    bill_amt3 = df['BILL_AMT3']
-    bill_amt4 = df['BILL_AMT4']
-    bill_amt5 = df['BILL_AMT5']
-    bill_amt6 = df['BILL_AMT6']
-    pay_amt1 = df['PAY_AMT1']
-    pay_amt2 = df['PAY_AMT2']
-    pay_amt3 = df['PAY_AMT3']
-    pay_amt4 = df['PAY_AMT4']
-    pay_amt5 = df['PAY_AMT5']
-    pay_amt6 = df['PAY_AMT6']
-    default = df['defaultPaymentNextMonth']
-'''
-
-    # print(df)
 
 if __name__ == "__main__":
     read_credit_card_file()
