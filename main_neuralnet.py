85888575a16566db9d8723e30486990846a7cc93
Magnus Grøndalen <magnus.bg@hotmail.com>
2019-11-05 13:56:45 +0100

    NeuralNetClassifier now workinggit add .

--- main_neuralnet.py
+++ main_neuralnet.py
@@ -9,15 +9,18 @@ runs the classifier class with data from the data package
 """
 
 xls_file = "default_credit_card_data.xls"
-nn_clf = NeuralNetClassifier()
+nn_clf = NeuralNetClassifier(
+                        n_hidden_neurons = 50,
+                        learning_rate = 0.00001,
+                        max_iter = 1000,
+                        tol = 1e-5,
+                        verbose = True
+                        )
+
 X, y = nn_clf.read_credit_card_file(xls_file)
 X_train, X_test, y_train, y_test = nn_clf.train_test_split(X, y, test_size=0.3, random_state=4)
-
 nn_clf.initialize_weights(X_train, y_train)
-
-max_iter = 1000
-learning_rate = 0.00001
-nn_clf.fit(X_train, y_train, max_iter, learning_rate )
+nn_clf.fit(X_train, y_train)
 
 # coef, loss_arr = clf.fit_data(X_train, y_train, learning_rate=0.1, n_iter=400)
 # pred = clf.predict(X_test)
